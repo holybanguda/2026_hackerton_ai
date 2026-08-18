334288469c6ecf9acdc9dd514d66531d1aa6c17c
holybanguda <yjkwon0826@naver.com>
2026-08-19 06:45:27 +0900

    hu

--- back/hackerton/src/main/java/com/hackerton/recommendation/RecommendationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hackerton.recommendation;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecommendationRepository extends JpaRepository<RecommendationEntity, Long> {
-}
